2176d86a829711e179fd0c1151185f5c1e0458e7
npondel <44888884+npondel@users.noreply.github.com>
2019-06-17 11:21:53 -0400

    Update stringmatch.R

--- stringmatch.R
+++ stringmatch.R
@@ -1,8 +1,8 @@
 #############################
 # Nick Pondel
-# June 12th, 2019
+# June 17th, 2019
 # String search script
-# Version 3
+# Version 4
 
 ## check packages before installing/running ##
 
@@ -26,6 +26,10 @@ if (!require(tibble)) {
   install.packages("tibble")
   library(tibble)
 }
+if (!require(readxl)) {
+  install.packages("readxl")
+  library(readxl)
+}
 
 # timing the script
 match.start.time <- Sys.time()
@@ -33,42 +37,38 @@ match.start.time <- Sys.time()
 ##### Read in data ######
 options(stringsAsFactors = FALSE)
 
-setwd("S:/RESLeonardJ_NickPondel/0_nick_final/")
-text_data <- read.csv(file = "5-combined_data/joined_records.csv")
+setwd("S:/RESLeonardJ_NickPondel/0_stringmatch_pipeline/input/")
+text_data <- read.csv(file = "text_data.csv")
 text_data$X <- NULL
 
-##### Define function for underscores #####
-under_func <- function(x){
-  y <- gsub("^", "_", x) #add at start of line
-  y <- gsub("$", "_", y) #add at end of line
+##### define function for text data underscores #####
+under_func_text <- function(x){
+  y <- gsub("^", "_", x) #add at start of word
+  y <- gsub("$", "_", y) #add at end of word
   y <- gsub(" ", "_", y) #replace all spaces
   return(y)
 }
 
 ##### Add underscores to main text data #####
-text_data$all_fields <- under_func(text_data$all_fields)
+text_data$all_fields <- under_func_text(text_data$all_fields)
 
-##### Pulling in word variables #####
-setwd("S:/RESLeonardJ_NickPondel/0_nick_final/6-single_word_search_variables/csv_6.7.19/")
-temp_list <- list.files()
+##### Reading in word variables #####
+word_variables <- lapply(excel_sheets("word_vars.xlsx"), read_excel,
+                         path = "word_vars.xlsx",
+                         col_names = FALSE)
+names(word_variables) <- excel_sheets("word_vars.xlsx")
 
-readcsv_func <- function(x) {
-  read.csv(file = x, header = F)$V1
+##### Define function for word variables underscores #####
+under_func_tibble <- function(x){
+  y <- x$...1 #create a vector by selecting the first (and only) column per tibble
+  y <- gsub("^", "_", y) #add at start of word
+  y <- gsub("$", "_", y) #add at end of word
+  y <- gsub(" ", "_", y) #replace all spaces
+  return(y)
 }
 
-word_variables <- lapply(temp_list, readcsv_func)
-
-var_names <- substring(temp_list, 21)
-var_names <- gsub(x = var_names, pattern = "*.csv", replacement = "")
-var_names <- gsub(x = var_names, pattern = " ", replacement = "_")
-names(word_variables) <- var_names
-
 ##### Add underscores to all word search variables #####
-word_vars_underscores <- lapply(word_variables, under_func)
-
-###### Create our data frame for matches ######
-match_data <- data.frame(text_data$KEY_CASE)
-names(match_data) <- "KEY_CASE"
+word_vars_underscores <- sapply(word_variables, under_func_tibble)
 
 ###### matching function ######
 match_func <- function(words, text_df){
@@ -84,22 +84,23 @@ match_func <- function(words, text_df){
   return(x)
   }
 
-###### matching variables to main text ######
-match_data <- sapply(word_variables, match_func, text_df = text_data)
+###### calling string match function on text data ######
+match_data <- sapply(word_vars_underscores, match_func, text_df = text_data)
+match_data <- data.frame(match_data)
 
 ##### Calculate means of each variable #####
-match_means <- gather(summarize_all(match_data, mean))
-match_means <- rename(match_means, c("key"="match_variable", "value"="mean"))
+match_means <- gather(summarise_all(match_data, mean))
+names(match_means) <- c("variable","mean")
 
-##### Add back identifiers #####
-match_data_with_keys <- match_data
-row.names(match_data_with_keys) <- text_data$KEY_CASE
+##### Add back identifiers to data #####
+IDs <- text_data$KEY_CASE
+match_data_ID <- cbind(IDs,match_data)
 
 #####  Write out the data  #####
-setwd("S:/RESLeonardJ_NickPondel/0_nick_final/")
-write.csv(match_data, file = "8-string_match_data/match_data.csv")
-write.csv(match_data_with_keys, file = "8-string_match_data/match_data_withkeys.csv")
-write.csv(match_means, file = "8-string_match_data/data_match_means.csv")
+setwd("S:/RESLeonardJ_NickPondel/0_stringmatch_pipeline/output/")
+write.csv(match_data, file = "match_data_NOid.csv")
+write.csv(match_data_ID, file = "match_data.csv")
+write.csv(match_means, file = "data_means.csv")
 
 ##### Print run time #####
 
