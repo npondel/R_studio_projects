4a34df6e237ad71f121640a5c6e7d0638454a19f
npondel <44888884+npondel@users.noreply.github.com>
2019-06-17 11:29:57 -0400

    Add files via upload

---
+++ counts.R
@@ -0,0 +1,34 @@
+##### text data #####
+setwd("S:/RESLeonardJ_NickPondel/0_nick_final/")
+text.data <- read.csv(file = "5-combined_data/joined_records.csv", stringsAsFactors = FALSE)
+
+text.counting <- data.frame(text=text.data$all_fields, stringsAsFactors = FALSE)
+
+##### single counts #####
+tidy_data <- text.counting %>%
+  unnest_tokens(word, text)
+
+single_word_list <- tidy_data %>%
+  count(word, sort=TRUE)
+
+single_word_list$n <- NULL
+
+##### bigrams #####
+bigrams_list <- text.counting %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2)
+
+bigrams_list <- bigrams_list %>%
+  count(bigram, sort=TRUE)
+
+bigrams_list$n <- NULL
+bigrams_list$bigram <- gsub(" ","_",bigrams_list$bigram, ignore.case = T)
+
+##### trigrams #####
+trigrams_list <- text.counting %>%
+  unnest_tokens(trigram, text, token = "ngrams", n = 3)
+
+trigrams_list <- trigrams_list %>%
+  count(trigram, sort=TRUE)
+
+trigrams_list$n <- NULL
+trigrams_list$trigram <- gsub(" ","_",trigrams_list$trigram, ignore.case = T)
